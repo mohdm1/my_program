8a7a7a9b42f6647fc876e468acc7c0de9b46029a
mohdm1 <mfirdaus.mohdaliff@gmail.com>
2017-04-06 21:02:22 -0400

    Update index.js
    added some test code

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 console.log("RUNNING RUNNING RUNNING")
+
+var d3 = require("d3") // assign the locally-installed D3 module to a variable called d3 for further invocation. 
+
+var someIntegers = [9, 13, 99, 3]
+console.log("THE ARRAY IS:", someIntegers)
+
+var maxNumber = d3.max(someIntegers)
+console.log("MAXIMUM NUMBER IN THE ARRAY IS:", maxNumber)
